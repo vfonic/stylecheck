c658b19cb9cc4fb7afa359dbfc0ab4e149e4aead
Viktor Fonic <viktor.fonic@gmail.com>
2019-04-06 15:02:20 +0800

    Fix: Disable Rails/DynamicFindBy on `Gem::Specification.find_by_name`
    * Because that's a not safe autocorrect
    
    https://github.com/rubocop-hq/rubocop/pull/6892

--- lib/stylecheck.rb
+++ lib/stylecheck.rb
@@ -6,7 +6,7 @@ require 'stylecheck/railtie' if defined?(Rails)
 module Stylecheck
   class << self
     def root
-      Gem::Specification.find_by(name: 'stylecheck').gem_dir
+      Gem::Specification.find_by_name('stylecheck').gem_dir # rubocop:disable Rails/DynamicFindBy
     end
   end
 end
